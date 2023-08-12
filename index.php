8ddae5aa832dac396fa55b7dddb9d2ecbc329ca1
Burmistrov Alexander <ab@piogroup.net>
2023-08-12 11:54:35 +0200

    new version 3

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ $p = [
 ///
 /// for ();
 
+
+// echo $a;
+$z = $c + $b;
+
 ?>
 <td><img src="images/<?php echo $p['image'] ?>" width="100px" height="100px" /></td>
 <td><?php echo $p['name'] ?></td>
